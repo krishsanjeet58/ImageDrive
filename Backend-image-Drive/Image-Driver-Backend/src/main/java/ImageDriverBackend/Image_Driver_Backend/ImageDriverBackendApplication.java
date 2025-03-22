c56ab568e14e3821d50ee46d2653c8b79d162b0a
krishsanjeet58 <choudarysanjeet58@gmail.com>
2025-03-22 14:21:20 +0530

    added frontend and backend

---
+++ Backend-image-Drive/Image-Driver-Backend/src/main/java/ImageDriverBackend/Image_Driver_Backend/ImageDriverBackendApplication.java
@@ -0,0 +1,13 @@
+package ImageDriverBackend.Image_Driver_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageDriverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageDriverBackendApplication.class, args);
+	}
+
+}
---
+++ Backend-image-Drive/Image-Driver-Backend/src/test/java/ImageDriverBackend/Image_Driver_Backend/ImageDriverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ImageDriverBackend.Image_Driver_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageDriverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
